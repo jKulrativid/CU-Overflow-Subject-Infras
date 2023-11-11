a85c1e01c2c84256b26718c6cbda37fd686b815a
K. Jetnipit <jetnipit54@gmail.com>
2023-11-11 19:23:19 +0700

    chore: create terraform for production

---
+++ terraform-product/ami.tf
@@ -0,0 +1,25 @@
+resource "aws_ami" "k8s_master_ami" {
+  name                = "k8s_master_ami"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
+  ebs_block_device {
+    device_name = "/dev/xvda"
+	volume_type = "gp2"
+    volume_size = 20
+	delete_on_termination = true
+  }
+}
+
+resource "aws_ami" "k8s_worker_ami" {
+  name                = "k8s_worker_ami"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
+  ebs_block_device {
+    device_name = "/dev/xvda"
+	volume_type = "gp2"
+    volume_size = 10
+	delete_on_termination = true
+  }
+}
---
+++ terraform-product/main.tf
@@ -0,0 +1,137 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = var.aws_region 
+}
+
+resource "aws_security_group" "k8s_master_sg" {
+  name        = "k8s-master-sg"
+  description = "Security group for Kubernetes master nodes"
+
+  ingress {
+    description = "Secure Shell Port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API Server Port"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Etcd Server Client Ports"
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubelet API Port"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kube-scheduler Port"
+    from_port   = 10259
+    to_port     = 10259
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kube-controller-manager Port"
+    from_port   = 10257
+    to_port     = 10257
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Cluster-wide Network Communication Port"
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "k8s-master-sg"
+  }
+}
+
+resource "aws_security_group" "k8s_worker_sg" {
+  name        = "k8s-worker-sg"
+  description = "Security group for Kubernetes worker nodes"
+
+  ingress {
+    description = "Secure Shell Port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubelet API Port"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "NodePort Services Ports"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Cluster-wide Network Communication Port"
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "k8s-worker-sg"
+  }
+
+}
+
+resource "aws_instance" "master" {
+  ami = aws_ami.k8s_master_ami.id
+  instance_type = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.k8s_master_sg.id]
+  tags = {
+    Name = "CU-Overflow-Master"
+  }
+}
